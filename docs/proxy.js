Gozala/lunet

commit d79245ce65b17ee7cbf2fbfe44b6d72688d854f5

Introduce `lunet.dev` host for local development & don't register SW when using it.

docs/proxy.js
@@ -7,7 +7,7 @@ import * as Data from "./lunet/data.js"
 const NAME = "proxy"
 const VERSION = "0.0.8"
 const ID = `${NAME}@${VERSION}`
-const lunetURL = new URL("https://lunet.link/")
+const lunetURL = new URL("/", self.location)
 const daemonURL = new URL("http://127.0.0.1:5001/")
 const gatewayURL = new URL("http://127.0.0.1:8080/")
 const embedURL = new URL("/embed.js", lunetURL)

docs/main.js
@@ -81,6 +81,16 @@ export class UserAgent {
     window.addEventListener("message", this)
   }
   async activateServiceWorker() {
+    // Disable service worker in dev setup as SW makes dev cycles
+    // a bit more complicated.
+    const { origin } = new URL(this.ownerDocument.location.href)
+    if (
+      origin === "https://lunet.dev" ||
+      origin === "http://localhost" ||
+      origin === "https://localhost"
+    ) {
+      return this.onStatusChange({ status: "ready" })
+    }
     const { serviceWorker } = this.ownerDocument.defaultView.navigator
     if (serviceWorker == null) {
       this.onStatusChange({ status: "unavailable" })

docs/service.js
@@ -3,7 +3,7 @@
 const NAME = "service"
 const VERSION = "0.0.11"
 const ID = `${NAME}@${VERSION}`
-const serviceURL = new URL("https://lunet.link/")
+const serviceURL = new URL("/", self.location)
 
 const install = (event /*:InstallEvent*/) => {
   console.log(`Proxy installed at ${self.registration.scope}`)

src/remote/main.js
@@ -17,6 +17,7 @@ const main = async port => {
     names: [
       "DNS:localhost",
       "DNS:lunet.link",
+      "DNS:lunet.dev",
       "DNS:*.lunet.link",
       "DNS:celestial.link",
       "DNS:*.celestial.link",

commit 0496756136b32bc043bbfecaa2d48263079753e1

Lunet URL is different from SW location
Fix regression introduced by d79245ce65b17ee7cbf2fbfe44b6d72688d854f5

docs/proxy.js
@@ -7,7 +7,7 @@ import * as Data from "./lunet/data.js"
 const NAME = "proxy"
 const VERSION = "0.0.8"
 const ID = `${NAME}@${VERSION}`
-const lunetURL = new URL("/", self.location)
+const lunetURL = new URL("https://lunet.link/")
 const daemonURL = new URL("http://127.0.0.1:5001/")
 const gatewayURL = new URL("http://127.0.0.1:8080/")
 const embedURL = new URL("/embed.js", lunetURL)